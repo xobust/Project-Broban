f3930526d6afc5f38f9200934fc1ca45a56448e6
willyliu <willyliu@kth.se>
2016-04-30 23:12:34 +0200

    Change a word in scoreboard.php

--- website/scoreboard.php
+++ website/scoreboard.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Project Broban - Download</title>
+<title>Project Broban - Scoreboard</title>
 <link rel="stylesheet" type="text/css" href="stylesheet.css">>
 </head>
 
