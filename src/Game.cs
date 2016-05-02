02a3cb133a41961e49e6d7025c833259a5481d79
Alexander Bladh <alexander.bladh@hotmail.com>
2016-05-02 22:58:26 +0200

    Renamed Game class from Game1 to GameManager

--- src/Game.cs
+++ src/Game.cs
@@ -7,12 +7,12 @@ namespace Project_Broban
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class GameManager : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public GameManager()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
