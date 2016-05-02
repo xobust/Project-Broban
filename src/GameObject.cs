60f2de18e314ab50d99710357e504d3f7724afba
Alexander Bladh <alexander.bladh@hotmail.com>
2016-05-02 22:55:02 +0200

    Created GameObject interface Issue: #5

---
+++ src/GameObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project_Broban
+{
+    interface GameObject
+    {
+        /// <summary>
+        /// Updates the state of the gameobject
+        /// </summary>
+        void Update();
+
+        /// <summary>
+        /// Renders the gameobject to the screen using the spritebatch
+        /// </summary>
+        void Render(SpriteBatch sb);
+    }
+}
