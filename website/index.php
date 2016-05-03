d25df7ae08b49bca8740d7ea8f8a2d0a71f9f6af
willyliu <willyliu@kth.se>
2016-05-03 11:18:22 +0200

    Implement banner on index page

--- website/index.php
+++ website/index.php
@@ -3,10 +3,22 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Project Broban - Home</title>
-<link rel="stylesheet" type="text/css" href="stylesheet.css">>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 
-<body>
-    <h1>Project Broban</h1>
+<body class="Home">
+    <ul class="menu">
+        <li><a href="">Home</a></li>
+        <li><a href="">Download</a></li>
+        <li><a href="">Scoreboard</a></li>
+    </ul>
+    <div class="banner">
+      <div class="bannerContent">
+        <h1>Project Broban</h1><br>
+        <p>
+            "Indie game of the year" -IGN
+        </p>
+      </div>
+    </div>
 </body>
 </html>
