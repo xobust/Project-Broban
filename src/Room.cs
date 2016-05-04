a7592982b8cceeae3e286b0ee4783c6c11bc247c
Robert Kindwall <dkdree@hotmail.com>
2016-05-04 17:03:25 +0200

    Add map creation loop in room :construction:

--- src/Room.cs
+++ src/Room.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 
 namespace Project_Broban
 {
     class Room
     {
+        private string[][] map;
+        private const int mapSizeX = 10;
+        private const int mapSizeY = 10;
 
+        public void Generate(SpriteBatch sb)
+        {
+            for(int x = 0; x < mapSizeX; x++)
+            {
+                for(int y = 0; y < mapSizeY; y++)
+                {
+
+                }
+            }
+        }
     }
 }
