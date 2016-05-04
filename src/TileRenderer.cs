aabb5309f88dd6ea18f1a4e09ca35d8c04a80c39
willyliu <willyliu@kth.se>
2016-05-04 17:29:59 +0200

    Update formatting and doc comments in TileRenderer :construction:

--- src/TileRenderer.cs
+++ src/TileRenderer.cs
@@ -11,16 +11,26 @@ namespace Project_Broban
 {
     class TileRenderer
     {
-        private Texture2D tileSet;  // The sprite sheet for the tiles
-        private const int tileHeight = 32;
-        private const int tileWidth = 64;
+        private Texture2D tileSet;                  // The sprite sheet for the tiles
+        private const int tileHeight = 32;          // The height of one tile
+        private const int tileWidth = 64;           // The width of one tile
         private const int tileOffset = tileWidth/2; // The position offset when placing diagonal tiles
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        public void LoadContent(GraphicsDevice gd, ContentManager cm)
+        {
+            //Temporary test texture
+            tileSet = cm.Load<Texture2D>("tileSet");
+        }
 
         /// <summary>
         /// Takes in a 2D array and draws the map.
         /// </summary>
-        /// <param name="sb">The SpriteBatch, passed in from the main Draw method</param>
-        /// <param name="map">The 2D array that represents the game map</param>
+        /// <param name="sb">The SpriteBatch, passed in from the main Draw method.</param>
+        /// <param name="map">The 2D array that represents the game map.</param>
         public void Draw(SpriteBatch sb, string[][] map)
         {
             for (int x = 0; x < map.Length; x++)
@@ -42,10 +52,10 @@ namespace Project_Broban
         /// <summary>
         /// Draws one tile at the specified position and the specified type.
         /// </summary>
-        /// <param name="sb">The SpriteBatch</param>
-        /// <param name="xPos">The x-coordinate of the tile</param>
-        /// <param name="yPos">The y-coordinate of the tile</param>
-        /// <param name="tileType">A string that represents the tile type</param>
+        /// <param name="sb">The SpriteBatch.</param>
+        /// <param name="xPos">The x-coordinate of the tile.</param>
+        /// <param name="yPos">The y-coordinate of the tile.</param>
+        /// <param name="tileType">A string that represents the tile type.</param>
         private void DrawTile(SpriteBatch sb, int xPos, int yPos, string tileType)
         {
             if (tileType == "0") return; // Empty tile
@@ -57,12 +67,6 @@ namespace Project_Broban
             //below this line
 
             sb.Draw(tileSet, destination, tileSource, Color.White);
-        }
-
-        public void LoadContent(GraphicsDevice gd, ContentManager cm)
-        {
-            //Temporary test texture
-             tileSet = cm.Load<Texture2D>("tileSet");
         }
 
         public void UnloadContent()
