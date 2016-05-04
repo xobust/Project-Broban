36a8c8984bda0a0bd4eab965177fc29e55520243
willyliu <willyliu@kth.se>
2016-05-04 14:09:50 +0200

    Add empty loop in the Draw function in TileRenderer :construction:

--- src/TileRenderer.cs
+++ src/TileRenderer.cs
@@ -12,22 +12,32 @@ namespace Project_Broban_win.src
 {
     class TileRenderer
     {
-        private Texture2D tileSet;
+        private Texture2D tileSet;  // The sprite sheet for the tiles
+        private const int tileSize = 32;
+        private const int tileOffset = tileSize/2; // The position offset when placing diagonal tiles
 
-        public void Update()
+        public void Draw(SpriteBatch sb, string[][] map)
         {
-            
-        }
+            for (int x = 0; x < map.Length; x++)
+            {
+                for (int y = 0; y < map[x].Length; y++)
+                {
+                    if (y % 2 == 0) // Even numbered tiles
+                    {
 
-        public void Draw(SpriteBatch sb)
-        {
+                    }
+                    else
+                    {
 
+                    }
+                }
+            }
         }
 
         public void LoadContent(GraphicsDevice gd, ContentManager cm)
         {
             //Temporary test texture
-             = cm.Load<Texture2D>("player");
+             tileSet = cm.Load<Texture2D>("tileSet");
         }
 
         public void UnloadContent()
