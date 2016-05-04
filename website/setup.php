3bc002d4ff2d3b6af4a1f6253c18c437a513adee
Alexander Bladh <alexander.bladh@hotmail.com>
2016-05-04 14:42:49 +0000

    Implement table creation script

--- website/setup.php
+++ website/setup.php
@@ -12,6 +12,16 @@ $pstatement = $mysql->prepare("CREATE DATABASE IF NOT EXISTS $dbname");
 $pstatment->execute();
 
 $database = new PDO("mysql:host=.$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD); 
+$table= "highscore";
+$columns = "ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(15) NOT NULL, Time INT(10) NOT NULL";
 
-
+$createTable = $db->exec("CREATE TABLE IF NOT EXISTS $DB_NAME.$table ($columns)");
+if ($createTable) 
+{
+    echo "Table $table - Created!<br /><br />";
+}    
+else 
+{ 
+    echo "Table $table not successfully created! <br /><br />";
+}
 ?>
