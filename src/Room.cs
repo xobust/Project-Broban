5a25469e8f39a148ad168dd6c5e420d1c598f483
Alexander Bladh <albladh@kth.se>
2016-05-08 15:04:43 +0200

    Add Room class

---
+++ src/Room.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Project_Broban
+{
+    class Room : GameObject
+    {
+        int XPossition;
+        int YPossition;
+
+
+
+        Room(int xPossition, int yPossition)
+        {
+            XPossition = xPossition;
+            YPossition = yPossition;
+        }
+
+        /// <summary>
+        /// Generates the world 
+        /// </summary>
+        public void Generate()
+        {
+
+        }
+        /// <summary>
+        /// Updates the state of the gameobject
+        /// </summary>
+        public void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// Renders the gameobject to the screen using the spritebatch
+        /// </summary>
+        public void Draw(SpriteBatch sb)
+        {
+
+        }
+        /// <summary>
+        /// Loads necessary content of the gameobject
+        /// </summary>
+        public void LoadContent(GraphicsDevice gd, ContentManager cm)
+        {
+
+        }
+
+        /// <summary>
+        /// Unloads content from the gameobject
+        /// </summary>
+        public void UnloadContent()
+        {
+
+        }
+    }
+}
