3a92cc11cbac14e80b572bb5feb1eac1505e2157
Robert Kindwall <dkdree@hotmail.com>
2016-05-11 16:53:12 +0200

    Make monster extend GameObject :construction:

--- src/Monster.cs
+++ src/Monster.cs
@@ -8,9 +8,9 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 
 
-namespace Project_Broban_win.src
+namespace Project_Broban
 {
-    class Monster
+    class Monster : GameObject
     {
         private const float size = 1;
         private const float speed = 1;
@@ -18,5 +18,29 @@ namespace Project_Broban_win.src
         private const float moveSpeed = 1;
         private const int damage = 1;
         Vector2 position;
+
+        public Monster(float x, float y)
+        {
+            position = new Vector2(x, y);
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            
+        }
+
+        public void LoadContent(GraphicsDevice gd, ContentManager cm)
+        {
+            
+        }
+
+        public void UnloadContent()
+        {
+        }
     }
 }
