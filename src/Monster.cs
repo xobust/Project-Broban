0c284a8b7400f8c35d5948fd208ae139e887b2b9
Robert Kindwall <dkdree@hotmail.com>
2016-05-11 23:29:13 +0200

    Remove hp as a constant :ok_hand:

--- src/Monster.cs
+++ src/Monster.cs
@@ -14,9 +14,9 @@ namespace Project_Broban
     {
         private const float size = 0.5f;
         private const float speed = 1;
-        private const int hp = 1;
         private const float moveSpeed = 1;
         private const int damage = 1;
+        private int hp = 1;
         private Texture2D texture;
         Vector2 position;
 
