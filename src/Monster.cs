6fabecb58c5093ccb065349d47b5aebc95084897
Robert Kindwall <dkdree@hotmail.com>
2016-05-11 16:41:41 +0200

    Add the monster class and variables :construction:

---
+++ src/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+
+namespace Project_Broban_win.src
+{
+    class Monster
+    {
+        private const float size = 1;
+        private const float speed = 1;
+        private const int hp = 1;
+        private const float moveSpeed = 1;
+        private const int damage = 1;
+        Vector2 position;
+    }
+}
