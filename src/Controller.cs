6563f84462057ef83c3ded49c4e8ccf696705fe6
Robert Kindwall <dkdree@hotmail.com>
2016-05-13 15:15:17 +0200

    Change namespace to project_broban :construction:

---
+++ src/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Broban
+{
+    interface Controller
+    {
+        /// <summary>
+        /// Updates the state of the gameobject
+        /// </summary>
+        void Update();
+    }
+}
