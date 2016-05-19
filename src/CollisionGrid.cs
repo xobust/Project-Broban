1599921d12ab2e9bd4dcf230998009e4a6287150
Robert Kindwall <dkdree@hotmail.com>
2016-05-19 17:28:23 +0200

    Complete the grid creation with zero values :construction:

--- src/CollisionGrid.cs
+++ src/CollisionGrid.cs
@@ -12,8 +12,10 @@ namespace Project_Broban
 
         public void GenerateGrid(int mapSizeX, int mapSizeY)
         {
+            grid = new int[mapSizeX][];
             for (int x = 0; x < mapSizeX; x++)
             {
+                grid[x] = new int[mapSizeY];
                 for (int y = 0; y < mapSizeY; y++)
                 {
                     grid[x][y] = 0;
