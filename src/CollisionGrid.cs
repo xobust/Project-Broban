d25d0c6e735ace54fdc185c05dbd374696990934
Robert Kindwall <dkdree@hotmail.com>
2016-05-19 16:55:45 +0200

    Add the creation of the grid :construction:

--- src/CollisionGrid.cs
+++ src/CollisionGrid.cs
@@ -8,5 +8,17 @@ namespace Project_Broban
 {
     class CollisionGrid
     {
+        public int[][] map;
+
+        public void GenerateGrid(int mapSizeX, int mapSizeY)
+        {
+            for (int x = 0; x < mapSizeX; x++)
+            {
+                for (int y = 0; y < mapSizeY; y++)
+                {
+                    map[x][y] = 0;
+                }
+            }
+        }
     }
 }
