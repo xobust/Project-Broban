e8322bea4b493dd7add599dc0e96f2c5615de9d8
Alexander Bladh <alexander.bladh@hotmail.com>
2016-05-19 14:05:33 +0200

    Add PlayerController :construction:

---
+++ src/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Project_Broban
+{
+    class PlayerController
+    {
+        GameManager GameManager;
+        Player player;
+
+        public PlayerController(GameManager gameManager)
+        {
+            this.GameManager = gameManager;
+            player = gameManager.player;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+    }
+}
