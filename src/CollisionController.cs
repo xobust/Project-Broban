8c2b1650fa81d0acae67c3458098be9f37620e66
Robert Kindwall <dkdree@hotmail.com>
2016-05-22 00:24:47 +0200

    Change the name of collisiongrid :construction:

---
+++ src/CollisionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Broban
+{
+    class CollisionController
+    {
+        public int[][] grid;
+        private CollisionTile currentTile;
+
+        public CollisionGrid()
+        {
+            currentTile = new CollisionTile(2);
+            GenerateGrid(10,10);
+        }
+
+        public void GenerateGrid(int mapSizeX, int mapSizeY)
+        {
+            grid = new int[mapSizeX][];
+            for (int x = 0; x < mapSizeX; x++)
+            {
+                grid[x] = new int[mapSizeY];
+                for (int y = 0; y < mapSizeY; y++)
+                {
+                    grid[x][y] = 0;
+                }
+            }
+        }
+
+    }
+}
