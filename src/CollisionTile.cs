2444bce6e0a640fecee19ced78aba29f7279d5e4
Robert Kindwall <dkdree@hotmail.com>
2016-05-21 15:04:49 +0200

    Add CollisionTile class and a basic skeleton :construction:

---
+++ src/CollisionTile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Project_Broban
+{
+    class CollisionTile
+    {
+        public Vector2 A, B, C, D;
+
+        public CollisionTile()
+        {
+        }
+
+        public void CalculatePositions()
+        {
+
+        }
+    }
+}
