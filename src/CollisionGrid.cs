c2cdefe181f0106d4b98c690a4640788ad0fb634
Robert Kindwall <dkdree@hotmail.com>
2016-05-21 14:37:42 +0200

    Add tile struct :construction:

--- src/CollisionGrid.cs
+++ src/CollisionGrid.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Project_Broban
 {
     class CollisionGrid
     {
         public int[][] grid;
+
+        public CollisionGrid()
+        {
+            GenerateGrid(10,10);
+        }
+
+        public struct tile
+        {
+            Vector2 A;
+            Vector2 B;
+            Vector2 C;
+            Vector2 D;
+        }
 
         public void GenerateGrid(int mapSizeX, int mapSizeY)
         {
