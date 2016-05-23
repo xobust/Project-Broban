785cc5db20bd0a0f5e758b6a51af461561d7eb4e
willyliu <willyliu@kth.se>
2016-05-23 13:05:42 +0200

    Update docs in Room :books:

--- src/Room.cs
+++ src/Room.cs
@@ -11,16 +11,24 @@ namespace Project_Broban
 {
     public class Room : GameObject
     {
-        int XPosition;
-        int YPosition;
-        private string[][] map;
-        private const int mapSizeX = 12;
-        private const int mapSizeY = 29;
-        public Monster[] monsters;
-        private Random rngGenerator;
-        private TextureManager Textures;
-        private TileRenderer Tiles;
+        int XPosition;                      // The x-coordinate of the room in the world map
+        int YPosition;                      // The y-coordinate of the room in the world map
+        private string[][] map;             // A 2D array of the tiles in the room
+        private const int mapSizeX = 12;    // The width of the room
+        private const int mapSizeY = 29;    // The height of the room
+        public Monster[] monsters;          // An array of all monsters in the room
+        private Random rngGenerator;        // Random number generator
+        private TextureManager Textures;    // Holds all the sprites
+        private TileRenderer Tiles;         // Renders the actual tile sprites
 
+        /// <summary>
+        /// Creates an empty room. The room will be filled/generated when 
+        /// the player enters it for the first time.
+        /// </summary>
+        /// <param name="xPosition">The world position of the room.</param>
+        /// <param name="yPosition">The world position of the room.</param>
+        /// <param name="tm">The TextureManager </param>
+        /// <param name="tr">The TileRenderer draws the tile sprites.</param>
         public Room(int xPosition, int yPosition, TextureManager tm, TileRenderer tr)
         {
             Textures = tm;
@@ -43,9 +51,8 @@ namespace Project_Broban
         }
 
         /// <summary>
-        /// Generates the room map
+        /// Generates the room map.
         /// </summary>
-        /// <returns></returns>
         public void Generate()
         {
             for (int x = 0; x < mapSizeX; x++)
@@ -58,6 +65,9 @@ namespace Project_Broban
             SpawnMonsters();
         }
 
+        /// <summary>
+        /// Spawns Monsters in the room in random locations.
+        /// </summary>
         public void SpawnMonsters()
         {
             for (int i = 0; i < monsters.Length; i++)
@@ -66,9 +76,11 @@ namespace Project_Broban
                                           rngGenerator.Next(0,400), Textures);
             }
         }
-           /// <summary>
-           /// Updates the state of the gameobject
-           /// </summary>
+
+        /// <summary>
+        /// Updates the state of the gameObject.
+        /// Updates all the currently alive monsters in the room.
+        /// </summary>
         public void Update(GameTime gameTime)
         {
             foreach (Monster monster in monsters)
@@ -81,8 +93,9 @@ namespace Project_Broban
         }
 
         /// <summary>
-        /// Renders the gameobject to the screen using the spritebatch
+        /// Renders the Room using the spriteBatch.
         /// </summary>
+        /// <param name="sb">The SpriteBatch to draw with.</param>
         public void Draw(SpriteBatch sb)
         {
             Tiles.Draw(sb, map);
@@ -96,14 +109,16 @@ namespace Project_Broban
         }
 
         /// <summary>
-        /// Loads necessary content of the gameobject
+        /// Loads necessary content of the gameObject.
         /// </summary>
+        /// <param name="gd">The GraphicsDevice.</param>
+        /// <param name="cm">The ContentManager (contains graphics).</param>
         public void LoadContent(GraphicsDevice gd, ContentManager cm)
         {
         }
 
         /// <summary>
-        /// Unloads content from the gameobject
+        /// Unloads content from the gameObject.
         /// </summary>
         public void UnloadContent()
         {
