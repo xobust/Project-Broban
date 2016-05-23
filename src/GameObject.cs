e52e8d0a401f32839bd3391a90ba18bf3466076b
willyliu <willyliu@kth.se>
2016-05-23 12:52:42 +0200

    Update docs in GameObject :books:

--- src/GameObject.cs
+++ src/GameObject.cs
@@ -13,22 +13,26 @@ namespace Project_Broban
     interface GameObject
     {
         /// <summary>
-        /// Updates the state of the gameobject
+        /// Updates the state of the gameObject
         /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         void Update(GameTime gameTime);
 
         /// <summary>
-        /// Renders the gameobject to the screen using the spritebatch
+        /// Renders the gameObject to the screen using the spriteBatch.
         /// </summary>
+        /// <param name="sb">The spriteBatch to draw with.</param>
         void Draw(SpriteBatch sb);
 
         /// <summary>
-        /// Loads necessary content of the gameobject
+        /// Loads necessary content of the gameObject.
         /// </summary>
+        /// <param name="gd">The GraphicsDevice from GameManager.</param>
+        /// <param name="cm">The ContentManager from GameManager.</param>
         void LoadContent(GraphicsDevice gd, ContentManager cm);
 
         /// <summary>
-        /// Unloads content from the gameobject
+        /// Unloads content from the gameObject.
         /// </summary>
         void UnloadContent();
     }
