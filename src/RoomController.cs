f2803e34f2d7db0119e427b3827a409832aa4e4c
Alexander Bladh <albladh@kth.se>
2016-05-24 16:04:24 +0200

    Add RoomController

---
+++ src/RoomController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Project_Broban
+{
+    class RoomController : Controller
+    {
+        Player player;
+        World world;
+
+        public RoomController(GameManager gameManager)
+        {
+            this.player = gameManager.player;
+            this.world = gameManager.GameWorld;
+        }
+
+        /// <summary>
+        /// Checks player position and moves him to a new room
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(GameTime gameTime)
+        {
+
+        } 
+        
+    }
+}
