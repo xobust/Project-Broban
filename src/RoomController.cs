3aa1c549afd2895989873b4588fb937b75255668
Robert Kindwall <dkdree@hotmail.com>
2016-05-25 01:17:33 +0200

    Add a roomcontroller and move enter room to it :construction:

---
+++ src/RoomController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Project_Broban
+{
+    class RoomController : Controller
+    {
+        private GameManager GameManager;
+
+        public RoomController(GameManager gameManager)
+        {
+            GameManager = gameManager;
+            EnterRoom(GameManager.GameWorld.CurrentXPosition, 
+                      GameManager.GameWorld.CurrentYPosition);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        /// <summary>
+        /// This function is executed when the player enters a room.
+        /// </summary>
+        /// <param name="x">X position for the room.</param>
+        /// <param name="y">Y position for the room.</param>
+        public void EnterRoom(int x, int y)
+        {
+            World world = GameManager.GameWorld;
+            if (world.WorldMap[x][y] == null)
+            {
+                world.WorldMap[x][y] = new Room(x, y, world.Textures, world.Tiles);
+                world.WorldMap[x][y].Generate();
+            }
+            world.CurrentXPosition = x;
+            world.CurrentYPosition = y;
+            world.currentRoom.Generate();
+            world.currentRoom = world.WorldMap[world.CurrentXPosition][world.CurrentYPosition];
+
+            GameManager.collisionController.GenerateCollisionMap();
+        }
+    }
+}
